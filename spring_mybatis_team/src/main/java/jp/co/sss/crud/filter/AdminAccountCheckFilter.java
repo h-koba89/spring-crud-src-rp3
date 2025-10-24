594d10fcc4b0967e7ac1d33a7b096e643bfc4d32
新實拓磨 <t_niinomi@braingate.co.jp>
2025-10-24 15:16:04 +0900

    fix:AdminAccountCheckFilter,LoginCheckFilter
    フィルター機能のクラスを改修

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/AdminAccountCheckFilter.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/AdminAccountCheckFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import jp.co.sss.crud.entity.Employee;
 
 /**
  * 権限認証用フィルタ
@@ -30,39 +33,44 @@ public class AdminAccountCheckFilter extends HttpFilter {
 		}
 
 		//TODO セッションからユーザー情報を取得
-
+		HttpSession session = (HttpSession) request.getSession().getAttribute("user");
 		//TODO セッションユーザーのIDと権限の変数をそれぞれ初期化
-
+		Integer id = 0;
+		Integer authority = 0;
 		//TODO セッションユーザーがNULLでない場合
-		if (false) {
+		if (session != null) {
 			//TODO セッションユーザーからID、権限を取得して変数に代入
-
+			Employee employee = (Employee) session.getAttribute("user");
+			id = employee.getEmpId();
+			authority = employee.getAuthority();
 		}
 
 		//TODO  更新対象の社員IDをリクエストから取得
-
+		String employeeId = request.getSession().getId();
+		Integer employee = 0;
 		//TODO  社員IDがNULLでない場合
-		if (false) {
+		if (employeeId != null) {
 			//TODO 社員IDを整数型に変換
+			employee = Integer.parseInt(employeeId);
 		}
 
 		//フィルター通過のフラグを初期化 true:フィルター通過 false:ログイン画面へ戻す
 		boolean accessFlg = false;
 
 		//TODO  管理者(セッションユーザーのIDが2)の場合、アクセス許可
-		if (false) {
+		if (authority == 2) {
 			accessFlg = true;
 			//TODO  ログインユーザ自身(セッションユーザのIDと変更リクエストの社員IDが一致)の画面はアクセス許可
-		} else if (false) {
+		} else if (id == employee) {
 			accessFlg = true;
 		}
 
 		//TODO  accessFlgが立っていない場合はログイン画面へリダイレクトし、処理を終了する
-		if (false) {
+		if (accessFlg) {
 			//TODO  レスポンス情報を取得
-
+			HttpServletResponse responsed = response;
 			//TODO  ログイン画面へリダイレクト
-
+			responsed.sendRedirect("/index");
 			//処理を終了
 			return;
 		}
--- spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/LoginCheckFilter.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/LoginCheckFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * ログインチェック用フィルタ
@@ -20,9 +21,11 @@ public class LoginCheckFilter extends HttpFilter {
 			throws IOException, ServletException {
 
 		//TODO セッションからユーザー情報を取得
-
+		HttpSession session = (HttpSession) request.getSession().getAttribute("user");
 		//TODO ユーザーがNULLの場合、ログイン画面にリダイレクトする
-
+		if (session == null) {
+			response.sendRedirect("/index");
+		}
 		// 次の処理へ移行
 		chain.doFilter(request, response);
 		return;
