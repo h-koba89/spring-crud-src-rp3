0ccae18b98402b5b462b41e11b36eecaef28e157
hkoba <hkoba@192.168.10.47>
2025-10-27 13:55:29 +0900

    Merge branch 'main' of https://github.com/h-koba89/spring-crud-src-rp3.git into main

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/AdminAccountCheckFilter.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/filter/AdminAccountCheckFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import jp.co.sss.crud.entity.Employee;
 
@@ -33,27 +32,30 @@ public class AdminAccountCheckFilter extends HttpFilter {
 		}
 
 		//TODO セッションからユーザー情報を取得
-		HttpSession session = (HttpSession) request.getSession().getAttribute("user");
+		Employee sessionUserEmployee = (Employee) request.getSession().getAttribute("user");
 		//TODO セッションユーザーのIDと権限の変数をそれぞれ初期化
 		Integer id = 0;
 		Integer authority = 0;
 		//TODO セッションユーザーがNULLでない場合
-		if (session != null) {
+		if (sessionUserEmployee != null) {
 			//TODO セッションユーザーからID、権限を取得して変数に代入
-			Employee employee = (Employee) session.getAttribute("user");
-			id = employee.getEmpId();
-			authority = employee.getAuthority();
+			//Employee employee = (Employee) session.getAttribute("user");
+			id = sessionUserEmployee.getEmpId();
+			authority = sessionUserEmployee.getAuthority();
 		}
-
 		//TODO  更新対象の社員IDをリクエストから取得
-		String employeeId = request.getSession().getId();
-		Integer employee = 0;
-		//TODO  社員IDがNULLでない場合
-		if (employeeId != null) {
-			//TODO 社員IDを整数型に変換
-			employee = Integer.parseInt(employeeId);
-		}
+		String employeeIdParam = request.getParameter("empId");
+		Integer targetEmpId = 0;
 
+		//TODO  社員IDがNULLでない場合
+		if (employeeIdParam != null) {
+			//TODO 社員IDを整数型に変換
+			try {
+				targetEmpId = Integer.parseInt(employeeIdParam);
+			} catch (NumberFormatException e) {
+				response.sendRedirect("/index");
+			}
+		}
 		//フィルター通過のフラグを初期化 true:フィルター通過 false:ログイン画面へ戻す
 		boolean accessFlg = false;
 
@@ -61,12 +63,12 @@ public class AdminAccountCheckFilter extends HttpFilter {
 		if (authority == 2) {
 			accessFlg = true;
 			//TODO  ログインユーザ自身(セッションユーザのIDと変更リクエストの社員IDが一致)の画面はアクセス許可
-		} else if (id == employee) {
+		} else if (id.equals(targetEmpId)) {
 			accessFlg = true;
 		}
 
 		//TODO  accessFlgが立っていない場合はログイン画面へリダイレクトし、処理を終了する
-		if (accessFlg) {
+		if (!accessFlg) {
 			//TODO  レスポンス情報を取得
 			HttpServletResponse responsed = response;
 			//TODO  ログイン画面へリダイレクト
